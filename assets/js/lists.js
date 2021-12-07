b8a614a9a1efd71f883539357011850fcd6ea4c2
Mank-2 <saqriphrx@gmail.com>
2021-12-08 02:00:26 +0800

    .

--- assets/js/lists.js
+++ assets/js/lists.js
@@ -25,7 +25,10 @@ const printFirstList = () => {
 // Print the second List
 if (CONFIG.lists.secondList[0].hasOwnProperty("image")) {
   const printSecondList = () => {
-    list_2.style.backgroundImage = `url(${CONFIG.lists.secondList[0].image})`;
+    list_2.style.push({
+      backgroundImage:`url(${CONFIG.lists.secondList[0].image})`, 
+      backgroundSize:'auto 100%'
+    });
   };
 } else {
   const printSecondList = () => {
