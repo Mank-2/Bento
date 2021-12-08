80ce4bbc32876493e997606cfd23e092ca1235e3
Mank-2 <saqriphrx@gmail.com>
2021-12-08 16:33:15 +0800

    .

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,21 +1,23 @@
-dropEventHandler = (ev) => {
-    ev.preventDefault();
+window.onload = () => {
+    dropEventHandler = (ev) => {
+        ev.preventDefault();
 
-    var imgFile = ev.dataTransfer.items[0].getAsFile();
-    // Perhaps this could be useful for later...
-    // var imgURL = URL.createObjectURL(imgFile); 
+        var imgFile = ev.dataTransfer.items[0].getAsFile();
+        // Perhaps this could be useful for later...
+        // var imgURL = URL.createObjectURL(imgFile); 
 
-    var fr = new FileReader;
+        var fr = new FileReader;
 
-    fr.onload = function() {
-        var img = new Image();
+        fr.onload = function() {
+            var img = new Image();
+            
+            localStorage.setItem(`${ev.target.id}_img`, fr.result);
+            document.getElementById(ev.target.id).style.backgroundImage = `url(${fr.result})`;
+        };
         
-        localStorage.setItem(`${ev.target.id}_img`, fr.result);
-        document.getElementById(ev.target.id).style.backgroundImage = `url(${fr.result})`;
+        fr.readAsDataURL(imgFile);
     };
-    
-    fr.readAsDataURL(imgFile);
-};
+}
 
 dragOverEventHandler = (ev) => {
     ev.preventDefault()
