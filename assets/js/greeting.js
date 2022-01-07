2f7d9df435c5353e7a56b685d0fff5fd02ec5509
Mank-2 <saqriphrx@gmail.com>
2022-01-08 05:49:15 +0800

    .

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -9,22 +9,23 @@ const hour = today.getHours();
 // Here you can change your name
 const name = CONFIG.name;
 
-// Here you can change your greetings
-const gree1 = `${CONFIG.greetingNight}\xa0`;
-const gree2 = `${CONFIG.greetingMorning}\xa0`;
-const gree3 = `${CONFIG.greetingAfternoon}\xa0`;
-const gree4 = `${CONFIG.greetingEvening}\xa0`;
+// Here you can change your greetings (\xa0)
+const gree1 = `${CONFIG.greetingNight}`;
+const gree2 = `${CONFIG.greetingMorning}`;
+const gree3 = `${CONFIG.greetingAfternoon}`;
+const gree4 = `${CONFIG.greetingEvening}`;
 
 // Define the hours of the greetings
 if (hour >= 23 || hour < 5) {
-  document.getElementById('greetings').innerText = gree1 + name;
+  document.getElementById('greetings').innerText = 
+    Config.Name ? gree4 : gree4 + "\xa0" + name;
 } else if (hour >= 6 && hour < 12) {
   document.getElementById('greetings').innerText =
-    gree2 + name;
+    Config.Name ? gree2 : gree2 + "\xa0" + name;
 } else if (hour >= 12 && hour < 17) {
   document.getElementById('greetings').innerText =
-    gree3 + name;
+    Config.Name ? gree3 : gree3 + "\xa0" + name;
 } else {
   document.getElementById('greetings').innerText =
-    gree4 + name;
+    Config.Name ? gree4 : gree4 + "\xa0" + name;
 }
