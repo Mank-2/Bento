e6cbefa9c95d0a16724d6406f0fe8fce87dbc901
Mank-2 <saqriphrx@gmail.com>
2022-01-08 05:51:29 +0800

    .\

--- assets/js/greeting.js
+++ assets/js/greeting.js
@@ -18,14 +18,14 @@ const gree4 = `${CONFIG.greetingEvening}`;
 // Define the hours of the greetings
 if (hour >= 23 || hour < 5) {
   document.getElementById('greetings').innerText = 
-    Config.Name ? gree4 : gree4 + "\xa0" + name;
+    name ? gree4 : gree4 + "\xa0" + name;
 } else if (hour >= 5 && hour < 12) {
   document.getElementById('greetings').innerText =
-    Config.Name ? gree2 : gree2 + "\xa0" + name;
+    name ? gree2 : gree2 + "\xa0" + name;
 } else if (hour >= 12 && hour < 17) {
   document.getElementById('greetings').innerText =
-    Config.Name ? gree3 : gree3 + "\xa0" + name;
+    name ? gree3 : gree3 + "\xa0" + name;
 } else {
   document.getElementById('greetings').innerText =
-    Config.Name ? gree4 : gree4 + "\xa0" + name;
+    name ? gree4 : gree4 + "\xa0" + name;
 }
